67f50804a9c2b5d6451230db02f22fc1a59f9d1a
Joao Lucas Aguiar Chaves <lucas2015nxi@gmail.com>
2022-02-21 11:59:12 -0400

    first commit

---
+++ AutomaticContracts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomaticContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Contract data");
+        }
+    }
+}
